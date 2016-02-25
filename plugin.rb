d2c8e98aab6b11fa8a627867f9bf8473789be7ff
Mark Nyon <mark@grandkru.com>
2016-02-24 23:49:57 -0500

    Initial commit for #1
    - assets/javascripts/discourse/initializers/alert.js.es6 - Alert
      initializer example. Not part of final product
    
    - config/locales/server.en.yml - English localization strings
    
    - config/settings.yml - Admin settings
    
    - plugin.rb - Discourse plugin file

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-customized-landing-page
+# about: A plugin that determines the landing page a user sees
+# when they log in, depending upon the group to which the user
+# belongs.
+# version: 0.0.1
+# authors: Mark Nyon
+enabled_site_setting :discourse_customized_landing_page
